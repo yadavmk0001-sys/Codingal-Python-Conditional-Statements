a32dacef047efa7b80662d3cce218963fa86fe69
yadavmk0001-sys <yadavmk0001@gmail.com>
2026-04-14 21:31:11 +0530

    Add files via upload

---
+++ Prog3.py
@@ -0,0 +1,10 @@
+i = int(input("Enter the number"))
+
+if (i < 15):
+    print("i is smaller than 15")
+    print("I'm in if block")
+else:
+    print("i is greater than 15")
+    print("I'm in else block")
+
+print("I'm in not in if and else block")
