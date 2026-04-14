6153e8e67274281c8358ac5d19c3d039568b2c7c
yadavmk0001-sys <yadavmk0001@gmail.com>
2026-04-14 21:38:54 +0530

    Add files via upload

---
+++ Prog4.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number to check : "))
+print("Number that user has inputed : ", number)
+
+if number%2==0:
+    print("It is an even number")
+else:
+    print("It is an odd number")
