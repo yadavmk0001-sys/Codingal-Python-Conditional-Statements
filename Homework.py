b36ba09085a176155f5c407afc77652313aa92d2
yadavmk0001-sys <yadavmk0001@gmail.com>
2026-04-16 18:04:08 +0530

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,7 @@
+temp = int(input("Enter the current weather : "))
+print("The current weather is ",temp ,"* C.")
+
+if temp <= 20:
+    print("You should wear woolen clothes.")
+else:
+    print("You should wear cotton clothes.")
